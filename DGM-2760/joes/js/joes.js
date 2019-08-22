ab8eabd087e7245a00bd5e35c3b44e0006afb1db
Bailey <bdalton100@yahoo.com>
2019-08-22 11:15:06 -0600

    finished joes

--- DGM-2760/joes/js/joes.js
+++ DGM-2760/joes/js/joes.js
@@ -1,10 +1,10 @@
-const header = document.querySelector('.header')
+const header = document.querySelector('.top')
 const dateObj = document.querySelector('.date')
 const name = document.querySelector('.name')
 
 const company = document.createElement('h1')
 company.className = 'companyName'
-company.textContent = 'Joes Bed & Breakfast'
+company.textContent = "Joe's Bed & Breakfast"
 header.appendChild(company)
 
 const slogan = document.createElement('h2')
@@ -19,12 +19,12 @@ const year = date.getFullYear()
 
 const newDate = document.createElement('p')
 newDate.className = 'date'
-newDate.textContent = month + '/' + day + '/' + year
+newDate.textContent = 'Today is ' + month + '/' + day + '/' + year
 dateObj.appendChild(newDate)
 
 let userName = document.createElement('p')
 userName.className = 'name'
-userName.textContent = 'Hello ' + prompt("What's your name?") + ', welcome to Joes.'
+userName.textContent = 'Hello ' + prompt("What's your name?") + ", welcome to Joe's."
 name.appendChild(userName)
 
 console.log(userName)
