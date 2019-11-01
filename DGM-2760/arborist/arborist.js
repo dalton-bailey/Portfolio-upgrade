1dae617557c4db7f2aba1979c0714af84432566a
Bailey <bdalton100@yahoo.com>
2019-11-01 13:03:12 -0600

    arborist

--- DGM-2760/arborist/arborist.js
+++ DGM-2760/arborist/arborist.js
@@ -13,7 +13,41 @@ const listTrees = () => {
     displayResults.innerHTML = `${treeList} <span>${trees.length} elments long</span>`
 }
 
+
 listTrees()
+
+const lowerTrees = trees.map(function(tree) {
+    return tree.toLowerCase()
+})
+
+//Make lowercase
+document.querySelector('#lowerCase').onclick = () => {
+    
+    const newListTrees = () => {
+        let newTreeList = ''
+        lowerTrees.forEach(tree => {
+            newTreeList += `${tree} <br>`
+        })
+        displayResults.innerHTML = `${newTreeList} <span>${trees.length} elments long</span>`
+    }
+    
+    newListTrees()
+
+    //Sort array a to z
+document.querySelector('#sortTrees').onclick = () => {
+    lowerTrees.sort()
+    newListTrees()
+}
+}
+
+//Sort array a to z
+document.querySelector('#sortTrees').onclick = () => {
+    trees.sort()
+    listTrees()
+}
+
+
+
 
 //Add Redwood to end of list
 document.querySelector('#add_redwood').onclick = () => {
@@ -38,13 +72,11 @@ document.querySelector('#remove_tree1').onclick = () => {
     )
 }
 
-//Find position of second tree in list
-const pos = trees.indexOf('pine')
 
 //Remove 2nd tree
 document.querySelector('#remove_tree2').onclick = () => {
     if (trees.length > 1) {
-        trees.splice(pos, 1)
+        trees.splice(1, 1)
         listTrees()
     }
     else (
@@ -63,16 +95,31 @@ document.querySelector('#remove_tree3').onclick = () => {
     )
 }
 
-//Sort array a to z
-document.querySelector('#sortTrees').onclick = () => {
-    trees.sort()
-    listTrees()
+
+
+
+//Display 3rd tree
+document.querySelector('#showName3').onclick = () => {
+    if (trees.length > 2) {
+        const treeThree = trees[2]
+        listTrees()
+        errorElement.textContent = `The third tree is ${treeThree}`
+    }
+    else (
+        errorElement.textContent = 'There is not a 3rd tree in the list. Try adding one.'
+    )
 }
 
-//Make lowercase
-const lowerTrees = trees.map(lowTree => { return lowTree.toLowerCase()})
-document.querySelector('#lowerCase').onclick = () => {
-    lowerTrees
+//Display 4th tree
+document.querySelector('#showName4').onclick = () => {
+    if (trees.length > 3) {
+        const treeFour = trees[3]
+        listTrees()
+        errorElement.textContent = `The fourth tree is ${treeFour}`
+    }
+    else (
+        errorElement.textContent = 'There is not a 4th tree in the list. Try adding one.'
+    )
+
 }
 
-console.log(lowerTrees)
