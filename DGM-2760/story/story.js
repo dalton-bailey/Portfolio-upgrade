b1667714824ec3b17b89a83e07ea7bb43426084f
Bailey <bdalton100@yahoo.com>
2019-11-18 21:33:42 -0700

    unit 9

--- DGM-2760/story/story.js
+++ DGM-2760/story/story.js
@@ -1,1 +1,9 @@
-const trees = ['Oak', 'Pine', 'aspen', 'Bald Cypress']
+function tellStory() {
+    // nounArray = querySelector('#noun')
+    // lowercase String split()
+    // repeat for adjectives & verbs
+
+    const myStory = `Once upon a time there were four ${nounArray[0]}s named Geroge, Bill, Harry, and Ron...`
+
+    // get a reference to the querySelector('#story') = myStory
+}
